f4453a3ecab3c1b380ea8bbe2ebc0e94cd0ba9eb
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-10-31 20:30:22 +0700

    add a file cpp if else condition
    menambahkan file if else.cpp

---
+++ If else-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{int marks;
+	cout<< "Enter yours marks";
+	cin>>marks;
+	
+	if(marks>=60 && marks <= 100)
+		{cout<<"Your are pass";
+		}
+	else
+		{cout<<"your are fail";
+		}
+	return 0;
+}
