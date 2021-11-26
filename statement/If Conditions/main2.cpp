4986c6acdd021c37dd537ebea8417d7074d7ffe8
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-11-26 20:36:43 +0700

    Rename main.cpp to main2.cpp

---
+++ statement/If Conditions/main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ int marks;
+  cout << "enter your marks"; //output
+  cin >>marks; //input nilai 
+  
+  if(marks>=60) << jika marks lebih atau sama dengan 60
+  	  {cout<< "You are pass";} //jika marksa lebih atau sama dengan 60 maka output nya "you are pass"
+  	  return 0;
+}
