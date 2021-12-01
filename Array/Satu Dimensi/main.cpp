609887f98171c2ef7d3c916667d23c670cb075b1
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-12-01 21:52:38 +0700

    array satu dimensi

---
+++ Array/Satu Dimensi/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   int arr[5] = {1, 2, 3, 4, 5};
+   int i;
+   for(i=0; i<5; i++)
+   {             //i buat nampilin indek aray ke berapa
+      cout<<"arr["<<i<<"] = "<<arr[i]<<"\n";
+   }
+   return 0;
+}
