5260f90401da598183c5eb6409855e0ef5e83a76
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-12-01 21:37:39 +0700

    perulangan menggunakan for

---
+++ LOOP/contoh 1/for/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	int nilai = 0;
+	int i=1;
+	
+	cout << "Masukan Angka =";
+	cin >> nilai;
+	
+	//for loop
+	
+	for(int i = 1; i <= 10; i++)
+	{
+		cout << endl << nillai << " * " << i<< " = " << nilai * i;
+	}
+	
+	return 0;
+}
