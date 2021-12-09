3e81f6181fa5ea9f7744ed957d5773a904af843a
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2021-12-10 06:53:05 +0700

    membuat inputan angka dan string

--- Object Oriented Programing/iostream/main.cpp
+++ Object Oriented Programing/iostream/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	char nama[100];
+	int umur;
+	cout << "Masukan nama anda :"; //meminta inputan string
+	cin.getline(nama, 100);
+	
+	cout << "masukan umur anda"; //meminta input umur atau angka
+	cin >> umur;
+	cout << "Helllo World! by - "<< nama << "(" << umur << ")";
+}
