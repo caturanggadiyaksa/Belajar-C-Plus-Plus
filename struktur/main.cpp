3f55df54d19a384d6dbbc186cb3f2ba1ef5f93a7
Catur Angga Adiyaksa <88980174+caturanggadiyaksa@users.noreply.github.com>
2022-01-20 10:56:16 +0700

    contoh struct c++
    update main cpp menambahkan contoh struct

--- struktur/main.cpp
+++ struktur/main.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+struct paket_internet
+{
+	string nama_paket;
+	int kuota_telpon;
+	int kuota_internet;
+}begadang;
+
+int main() 
+{ 
+	begadang.nama_paket = "Begadang";
+	begadang.kuota_telpon = 50;
+	begadang.kuota_internet = 25; 
+	cout<<"Nama paket : "<< begadang.nama_paket << endl;
+	cout<<"Kuota telpon : "<< begadang.kuota_telpon << " Menit" << endl;
+	cout<<"Nama internet : "<< begadang.kuota_internet << " GB" << endl;
+
+	return 0;
+	
+}
